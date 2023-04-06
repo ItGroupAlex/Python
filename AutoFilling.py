86cfc1c651989258b79a269484e1daa64d298e48
Alex <125738088+ItGroupAlex@users.noreply.github.com>
2023-04-07 00:31:45 +0300

    Update AutoFilling.py

--- AutoFilling.py
+++ AutoFilling.py
@@ -5,6 +5,7 @@
 
 import psycopg2
 import names
+import random
 
 try:
     # connect to exist database
@@ -26,27 +27,91 @@ try:
 
         print(f"Server version: {cursor.fetchone()}")
 
-    # insert data into a table
+
+
+
+
+# insert data into a table employees
+    #70 random employee_name's
     with connection.cursor() as cursor:
-        for i in range(1, 2):
+        for i in range(1, 70):
             employee_name = names.get_full_name()
             cursor.execute(
-                """ 
-                INSERT INTO employees (employee_name) 
+                """
+                INSERT INTO employees (employee_name)
                 values (%s);
                 """,
                 [employee_name]
             )
             print("[INFO]"+"Insert of "+employee_name + "is added")
 
+# counter (def), insert data into a table employee_salary
+    #   №1-30(z)         random unique employee_id's from massive (1-70)
+    #   №30(z)-40(z1)    random unique employee_id's from massive (71-1000)
+    #       + random salary_id
+
+    z = 30
+    z1 = 40
+
+    with connection.cursor() as cursor:
+        def count():
+            cursor.execute(
+                """
+                SELECT COUNT(*) FROM employee_salary
+                """)
+            return (cursor.fetchone()[0])
+
+
+
+        l = random.sample(range(1, 70), z)
+        print(l)
+
+        counter = count()
+
+        m = 0
+        while counter < z:
+            employee_id = l[m]
+            salary_id = random.randint(0, 70)
+            cursor.execute(
+                """
+                INSERT INTO employee_salary (employee_id, salary_id)
+                values (%s, %s);
+                """,
+                (employee_id, salary_id)
+            )
+            print("[INFO]" + "Insert of " + str(employee_id) + " (employee_id) is added")
+            m += 1
+            counter = count()
+        print(counter)
+
+
+        l1 = random.sample(range(71, 1000), z1-z)
+        print(l1)
+
+        m = 0
+        while counter < z1:
+            employee_id = l1[m]
+            salary_id = random.randint(0, 70)
+            cursor.execute(
+                """
+                INSERT INTO employee_salary (employee_id, salary_id)
+                values (%s, %s);
+                """,
+                (employee_id, salary_id)
+            )
+            print("[INFO]" + "Insert of " + str(employee_id) + " (employee_id) is added")
+            m += 1
+            counter = count()
+        print(counter)
 
 
 
 
 
-
+# error SQL
 except Exception as _ex:
     print("[INFO] Error while working with PostgreSQL", _ex)
+# close connection
 finally:
     if connection:
         # cursor.close()
